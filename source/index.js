baf233f14dad78671dfdaeae6262636b4ff6f229
fmihel <fmihel76@gmail.com>
2021-09-27 16:25:45 +0300

    add ComboBox to index.js

--- source/index.js
+++ source/index.js
@@ -2,7 +2,8 @@ import Edit from './Edit/Edit.jsx';
 import Btn from './Btn/Btn.jsx';
 import CheckBox from './CheckBox/CheckBox.jsx';
 import Label from './Label/Label.jsx';
+import ComboBox from './ComboBox/ComboBox.jsx';
 
 export default {
-    Edit, Btn, CheckBox, Label,
+    Edit, Btn, CheckBox, Label, ComboBox,
 };
