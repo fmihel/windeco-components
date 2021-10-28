50a4e2d2215d3596c0d78908295e5cdcf527f714
fmihel <fmihel76@gmail.com>
2021-10-28 11:12:40 +0300

    fix ComboBoxItem

--- source/ComboBox/ComboBoxItem.jsx
+++ source/ComboBox/ComboBoxItem.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 export default class ComboBoxItem extends React.Component {
     render() {
         const disabled = this.props.disabled ? { disabled: true } : {};
-        return <option id={this.props.id} {...disabled}>{this.props.caption}</option>;
+        return <option value = {this.props.id} {...disabled}>{this.props.children}</option>;
     }
 }
