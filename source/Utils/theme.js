b7a93d66844c75de814ef4ff9e07d8cabff61493
fmihel <fmihel76@gmail.com>
2022-12-16 21:48:47 +0300

    combo and edit

--- source/Utils/theme.js
+++ source/Utils/theme.js
@@ -1,77 +1,51 @@
 class Theme {
-
     constructor() {
-
         this.themes = ['dark', 'light'];
-
     }
 
-
-
     get() {
-
         let out = false;
 
         const html = document.documentElement;
 
         const { themes } = this;
 
-
-
         html.className.split(' ').find((name) => {
-
             const idx = themes.indexOf(name);
 
             if (idx >= 0) {
-
                 out = themes[idx];
 
                 return true;
-
             }
 
             return false;
-
         });
 
-
-
         return out;
-
     }
 
-
-
     toggle(defalt = 'light') {
-
         const current = this.get();
 
         let theme = defalt;
 
         if (current) {
-
             let index = this.themes.indexOf(current);
 
             if (index >= 0) {
-
                 index += 1;
 
                 if (index >= this.themes.length) index = 0;
 
                 theme = this.thems[index];
-
             }
-
         }
 
         this.set(theme);
-
     }
 
-
-
     set(theme) {
-
         const html = document.documentElement;
 
         const { themes } = this;
@@ -79,12 +53,7 @@ class Theme {
         const clear = html.className.split(' ').filter((name) => (!(themes.indexOf(name) >= 0))).join(' ').trim();
 
         html.className = (clear ? ' ' : '') + theme;
-
     }
-
 }
 
-
-
 export default new Theme();
-
